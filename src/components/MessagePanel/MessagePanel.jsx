93d1bfe644b897f7f27c5395d859d5ed20eed5f0
Wutras <wunsch.jonas@hotmail.com>
2021-03-13 11:36:18 +0100

    Add automatic jump to newest message
    This way you'll always see the newest message and won't potentially be confused when you don't immediately see the reply to your question
    because you'd have to scroll down yourself.

--- src/components/MessagePanel/MessagePanel.jsx
+++ src/components/MessagePanel/MessagePanel.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { Message, RichMessage } from "..";
 import "./MessagePanel.css";
 
-export default function MessagePanel(props) {
-  return (
-    <div tabIndex="-1" id="messagePanel">
-      {props.messages.map((message, i) =>
-        // i is the shortest unique identifier in this case and the content will not be shuffled, so i is constant
-        !!message.visualization ? (
-          <RichMessage
-            key={i}
-            messageObject={message}
-          />
-        ) : (
-          <Message key={i} messageObject={message} />
-        )
-      )}
-    </div>
-  );
+export default class MessagePanel extends Component {
+  componentDidUpdate() {
+    window.requestAnimationFrame(() => {
+      const messagePanel = document.getElementById("messagePanel");
+      messagePanel.scrollTop += messagePanel.scrollHeight;
+    });
+  }
+
+  render() {
+    return (
+      <div tabIndex="-1" id="messagePanel">
+        {this.props.messages.map((message, i) =>
+          // i is the shortest unique identifier in this case and the content will not be shuffled, so i is constant
+          !!message.visualization ? (
+            <RichMessage key={i} messageObject={message} />
+          ) : (
+            <Message key={i} messageObject={message} />
+          )
+        )}
+      </div>
+    );
+  }
 }
