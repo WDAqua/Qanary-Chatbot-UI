73c0d43604165cd6426fa9fc19a140757c9b53df
Wutras <wunsch.jonas@hotmail.com>
2021-03-14 17:31:13 +0100

    Add max width to diagram
    The diagram's width will now cap at 800px.

--- src/components/Diagram/Diagram.jsx
+++ src/components/Diagram/Diagram.jsx
@@ -40,20 +40,24 @@ export default class Diagram extends Component {
   }
 
   render() {
-    const width =
-      parseInt(document.querySelector(".richContentContainer")?.clientWidth) * 0.6 ?? 300;
+    const width = Math.min(
+      parseInt(document.querySelector(".richContentContainer")?.clientWidth) *
+        0.6 ?? 300,
+      800
+    );
+    console.log(width);
     // pick height at 2/5th the width to create a nice landscape ratio
     const height = (width * 2) / 5;
     return (
       <div>
-      <svg
-        tabIndex="0"
-        className="diagram"
-        id={this.props.id}
-        width={`${width}`}
-        height={`${height}`}
-      />
-      <div className="diagramTitle">{this.props.diagramData.title}</div>
+        <svg
+          tabIndex="0"
+          className="diagram"
+          id={this.props.id}
+          width={`${width}`}
+          height={`${height}`}
+        />
+        <div className="diagramTitle">{this.props.diagramData.title}</div>
       </div>
     );
   }
